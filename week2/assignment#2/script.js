187c26d82097d98c8608c661a9ca34d9fa2e64b7
emilylyy <yingyi.emily@gmail.com>
2024-02-06 23:42:25 -0500

    assignment#2

---
+++ week2/assignment#2/script.js
@@ -0,0 +1,13 @@
+function changeLight(){
+    document.getElementById('light').style.color = "black"
+    document.getElementById('light').style.backgroundColor = "white"
+    document.getElementById('dark').style.color = "black"
+    document.getElementById('dark').style.backgroundColor = "white"
+}
+
+function changeDark(){
+    document.getElementById('light').style.color = "white"
+    document.getElementById('light').style.backgroundColor = "black"
+    document.getElementById('dark').style.color = "white"
+    document.getElementById('dark').style.backgroundColor = "black"
+}
